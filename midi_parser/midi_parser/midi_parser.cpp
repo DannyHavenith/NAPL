c8abf957263f9c1ce9a6a3da072f14712432652f
Danny Havenith <dh.github.com@havenith.net>
2009-09-06 03:06:14 +0000

    parser works on single test file. built a text-visitor.
    git-svn-id: svn://thuis/trunk/Sounds@467 3a7aa2ed-2c50-d14e-a1b9-f07bebb4988c

--- midi_parser/midi_parser/midi_parser.cpp
+++ midi_parser/midi_parser/midi_parser.cpp
@@ -2,6 +2,10 @@
 //
 #include <string>
 #include <vector>
+#include <iostream>
+#include <fstream>
+#include <iterator>
+#include <algorithm>
 
 #include <boost/config/warning_disable.hpp>
 #include <boost/spirit/include/qi.hpp>
@@ -11,8 +15,10 @@
 #include <boost/fusion/include/adapt_struct.hpp>
 #include <boost/fusion/include/io.hpp>
 #include <boost/variant/recursive_variant.hpp>
+#include <boost/variant/apply_visitor.hpp>
 #include <boost/spirit/include/qi_binary.hpp>
 
+#include <boost/lambda/lambda.hpp>
 #include "subsection_parser.hpp"
 
 //using namespace boost::phoenix;
@@ -20,38 +26,210 @@ using namespace boost::spirit;
 using namespace boost::spirit::qi;
 using namespace boost::spirit::ascii; // for a definition of space_type
 using namespace boost::spirit::arg_names;
+using namespace std;
 
-struct midi_meta_event
+namespace events
 {
+struct note
+{
+    unsigned char number;
+    unsigned char velocity;
 };
 
-struct midi_sysex_event
+
+struct note_off : note
+{};
+
+struct note_on : note
+{};
+
+struct note_aftertouch : note
+{};
+
+struct controller 
 {
+    unsigned char which;
+    unsigned char value;
 };
 
-struct simple_midi_event
+struct program_change
+{
+    program_change &operator=( int rhs)
+    {
+        program = rhs;
+    }
+    unsigned char program;
+};
+
+struct channel_aftertouch
+{
+    unsigned char value;
+};
+struct pitch_bend
+{
+    unsigned short value;
+};
+
+typedef boost::variant<
+    note_on,
+    note_off,
+    note_aftertouch,
+    controller,
+    program_change,
+    channel_aftertouch,
+    pitch_bend> channel_event;
+
+struct meta
+{
+    unsigned char       type;
+    std::vector<char>   bytes;
+};
+
+struct sysex
 {
 };
 
 typedef boost::variant<
-    midi_meta_event,
-    midi_sysex_event,
-    simple_midi_event
-> midi_event_variant;
+    meta,
+    sysex,
+    channel_event
+> any;
 
-struct midi_event
+template<typename Derived>
+struct visitor : public boost::static_visitor<>
+{
+    Derived &derived()
+    {
+        return *static_cast<Derived*>(this);
+    }
+
+    const Derived &derived() const
+    {
+        return *static_cast<Derived*>(this);
+    }
+
+    void operator()( const any &event)
+    {
+        boost::apply_visitor( derived(), event);
+    }
+
+    void operator()( const channel_event &event)
+    {
+        boost::apply_visitor( derived(), event);
+    }
+
+    void operator()( const meta &)
+    {
+    }
+
+    void operator()( const sysex &)
+    {
+    }
+
+    void operator()( const note_on &){}
+    void operator()( const note_off &){}
+    void operator()( const note_aftertouch &){}
+    void operator()( const controller &){}
+    void operator()( const program_change &){}
+    void operator()( const channel_aftertouch &){}
+    void operator()( const pitch_bend &){}
+};
+
+} // namespace events
+
+struct timed_midi_event
 {
     unsigned delta_time;
-    midi_event_variant event;
+    events::any event;
+};
+
+template<typename Derived>
+struct timed_midi_visitor : public events::visitor<Derived>
+{
+    timed_midi_visitor()
+        :current_time(0)
+    {
+
+    }
+
+    using events::visitor<Derived>::operator();
+    void operator()( const timed_midi_event &event)
+    {
+        current_time += event.delta_time;
+        derived()( event.event);
+    }
+
+    void reset()
+    {
+        current_time = 0;
+    }
+
+    size_t current_time;
+};
+
+struct print_text_visitor : public timed_midi_visitor<print_text_visitor>
+{
+    using timed_midi_visitor<print_text_visitor>::operator();
+    void operator()( const events::meta &event)
+    {
+        if (event.type == 0x05 || (event.type == 0x01))
+        {
+            std::cout << current_time << '\t' << std::string( event.bytes.begin(), event.bytes.end()) << '\n';
+        }
+    }
 };
 
 BOOST_FUSION_ADAPT_STRUCT(
-    midi_event,
-    (unsigned, delta_time)
-    (midi_event_variant, event)
+    events::note_on,
+    (unsigned char, number)
+    (unsigned char, velocity)
     )
 
-typedef std::vector< midi_event>   midi_track;
+BOOST_FUSION_ADAPT_STRUCT(
+    events::meta,
+    (unsigned char, type)
+    (std::vector<char>, bytes)
+    )
+
+BOOST_FUSION_ADAPT_STRUCT(
+    events::note_off,
+    (unsigned char, number)
+    (unsigned char, velocity)
+    )
+BOOST_FUSION_ADAPT_STRUCT(
+    events::note_aftertouch,
+    (unsigned char, number)
+    (unsigned char, velocity)
+    )
+
+BOOST_FUSION_ADAPT_STRUCT(
+    events::controller,
+    (unsigned char, which)
+    (unsigned char, value)
+    )
+
+BOOST_FUSION_ADAPT_STRUCT(
+    events::program_change,
+    (unsigned char, program)
+    )
+
+BOOST_FUSION_ADAPT_STRUCT(
+    events::channel_aftertouch,
+    (unsigned char, value)
+    )
+
+BOOST_FUSION_ADAPT_STRUCT(
+    events::pitch_bend,
+    (unsigned short, value)
+    )
+
+BOOST_FUSION_ADAPT_STRUCT(
+    timed_midi_event,
+    (unsigned, delta_time)
+    (events::any, event)
+    )
+
+typedef std::vector< timed_midi_event>   midi_track;
 
 struct midi_header
 {
@@ -82,10 +260,18 @@ BOOST_FUSION_ADAPT_STRUCT(
 template<typename Iterator>
 struct midi_parser: grammar< Iterator, midi_file()>
 {
-    midi_parser() : midi_parser::base_type( file)
+    midi_parser() : 
+        midi_parser::base_type( file),
+            running_status(-1)
     {
+        using boost::phoenix::val;
+//        using boost::phoenix::var;
+        using boost::phoenix::ref;
+        using boost::lambda::var;
+        using boost::lambda::constant;
+
         file
-            %= header >> +track
+            %= header >> *track
             ;
 
         header
@@ -102,45 +288,95 @@ struct midi_parser: grammar< Iterator, midi_file()>
 
         event
             %=
-                midi_event_r
-            |   sysex_event
+                sysex_event
             |   meta_event
+            |   channel_event
             ;
 
         meta_event
-            = byte(0xff) >> variable_length_quantity[ _a = _1] >> subsection(_a)[*byte]
+            %= char_('\xff') >> byte/*type*/ >> omit[variable_length_quantity[ _a = _1]] >> subsection(_a)[*byte]
             ;
 
         sysex_event
-			= (byte(0xfe) | byte(0xf7)) >> variable_length_quantity[ _a = _1] >> subsection(_a)[*byte]
+			= (char_('\xfe') | char_('\xf7')) >> variable_length_quantity[ _a = _1] >> subsection(_a)[*byte]
 			;
 
         variable_length_quantity
-            = eps[_val = 0] >> *high_byte[_val += _1] >> low_byte[_val += _1]
+            = eps[_val = 0] >> *high_byte[_val = (_val << 7) + (_1 & 0x7f)] >> low_byte[_val = (_val << 7) + _1]
             ;
 
-        using boost::phoenix::val;
+        // local _a contains the event type (top nibble of event code)
+        // if there's a high_byte, that will be the event/channel
+        // if there isn't, we use the running status (the previously seen event).
+        channel_event
+            =      omit[-high_byte[ref(running_status) = _1][ref(std::cout) << "new channel:" << running_status ]] >> eps[_a = val(running_status)/16] >> eps[std::cout << ref(running_status) << std::endl] >>
+        
+                    (
+                        eps( _a == 0x8) > note_off_event
+                    |   eps( _a == 0x9) > note_on_event
+                    |   eps( _a == 0xa) > note_aftertouch_event
+                    |   eps( _a == 0xb) > controller_event
+                    |   eps( _a == 0xc) > program_change_event
+                    |   eps( _a == 0xd) > channel_aftertouch_event
+                    |   eps( _a == 0xe) > pitch_bend_event
+                    )
+            ;
+
+        note_off_event
+            %=  byte >> byte
+            ;
+        
+        note_on_event
+            %=  byte >> byte
+            ;
+        
+        note_aftertouch_event
+            %=  byte >> byte
+            ;
+        
+        controller_event
+            %=  byte >> byte
+            ;
+        
+        program_change_event
+            %=  eps >> byte 
+            ;
+        
+        channel_aftertouch_event
+            %=  eps >> byte
+            ;
+
+        pitch_bend_event
+            %=  eps >> little_word;
+            ;
+
         low_byte
-            %= char_( val(0), val(127));
+            %= char_( '\x00', '\x7f');
 
         high_byte
-            %= char_( val(128), val(255));
-
-
+            %= char_( '\x80', '\xff');
     }
 
-    rule<Iterator, int()>                 high_byte;
-    rule<Iterator, int()>                 low_byte;
+    int running_status;
+    rule<Iterator, int()                > high_byte;
+    rule<Iterator, int()                > low_byte;
     rule<Iterator, midi_file()          > file;
     rule<Iterator, midi_header()        > header;
     rule<Iterator, midi_track(size_t)   > track_data;
-    rule<Iterator, midi_event()         > event_group;
-    rule<Iterator, midi_event_variant() > event;
-    rule<Iterator, simple_midi_event()  > midi_event_r;
-    rule<Iterator, midi_track(),        locals<size_t> > track;
-    rule<Iterator, midi_sysex_event(),  locals<size_t> > sysex_event;
-    rule<Iterator, midi_meta_event(),   locals<size_t> > meta_event;
-    rule<Iterator, size_t()            > variable_length_quantity;
+    rule<Iterator, timed_midi_event()   > event_group;
+    rule<Iterator, events::any()        > event;
+    rule<Iterator, events::pitch_bend() > pitch_bend_event;
+    rule<Iterator, size_t()             > variable_length_quantity;
+    rule<Iterator, events::note_off()       > note_off_event;
+    rule<Iterator, events::note_on()        > note_on_event;
+    rule<Iterator, events::note_aftertouch()> note_aftertouch_event;
+    rule<Iterator, events::controller()     > controller_event;
+    rule<Iterator, events::program_change() > program_change_event;
+    rule<Iterator, events::channel_aftertouch()> channel_aftertouch_event;
+    rule<Iterator, midi_track(),            locals<size_t>  > track;
+    rule<Iterator, events::sysex(),         locals<size_t>  > sysex_event;
+    rule<Iterator, events::meta(),          locals<size_t>  > meta_event;
+    rule<Iterator, events::channel_event(), locals<int>     > channel_event;
 };
 
 template<typename Expr>
@@ -163,18 +399,45 @@ struct sub : grammar<Iterator, std::vector<int>(), locals<size_t> >
 int main(int argc, char* argv[])
 {
     using namespace std;
-    string duck("\002\002\003\004");
+
+    if (argc < 2)
+    {
+        cerr << "usage: parsemidi <filename>\n";
+        return -1;
+    }
+
+    ifstream in(argv[1], ios::binary);
+    in.unsetf( ios_base::skipws);
+
+    if (!in)
+    {
+        cerr << "could not open " << argv[1] << '\n';
+        return -2;
+    }
+//    typedef boost::spirit::multi_pass<istream_iterator<char> > default_multi_pass_t;
+    typedef std::istreambuf_iterator<char> base_iterator_type;
+//    typedef multi_pass<base_iterator_type, unused_type> iterator;
+
+    std::string buffer;
+    buffer.assign( base_iterator_type(in), base_iterator_type());
+    std::cout << buffer.size() << "\n";
+    
     typedef string::iterator iterator;
 
-//    sub<iterator> sub_parser;
-    midi_parser<iterator> sub_parser;
+    midi_parser<iterator> parser;
+    midi_file contents;
 
-//    std::vector<int> result;
+    iterator first = buffer.begin();
+    iterator last = buffer.end();
+    boost::spirit::qi::parse( first, last, parser, contents);
 
-    boost::spirit::qi::parse( duck.begin(), duck.end(), sub_parser);
-
-  //  std::cout << result.size() << ' ' << result[0] << ' ' << result[1] << std::endl;
-
+    typedef std::vector< midi_track>::iterator track_iterator;
+    for (track_iterator i = contents.tracks.begin(); i != contents.tracks.end(); ++i)
+    {
+        print_text_visitor v;
+        std::for_each( i->begin(), i->end(), v);
+    }
+/**/
 	return 0;
 }
 
